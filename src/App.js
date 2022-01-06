d50b41a093f84d67194b8ebbe018d237fe865649
randy <baumer.randy@gmail.com>
2022-01-06 18:31:43 -0500

    added the beginning of the gallery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "Commercial",
+      description:
+        "Photo's of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "Portraits", description: "Portraits of people in my life" },
+    { name: "Food", description: "Delicious Delicacies" },
+    {
+      name: "Landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        ></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>  
     </div>
